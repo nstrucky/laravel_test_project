77c3ff8f9e75708076d995fd1b2e08081052e6c0
nstrucky <nickstruckmeyer@gmail.com>
2018-12-10 17:18:54 -0500

    css changes

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -9,15 +9,15 @@
 		{{ csrf_field() }}
 
 		<div>
-			<input type="text" class="input {{ $errors->has('first_name') ? 'is-danger' : '' }}" name="first_name" placeholder="First Name" value="{{old('first_name')}}" required>
+			<input type="text" class="input{{ $errors->has('first_name') ? '-is-danger' : '' }}" name="first_name" placeholder="First Name" value="{{old('first_name')}}" required>
 		</div>
 
 		<div>
-			<input type="text" name="last_name" placeholder="Last Name" value="{{old('last_name')}}" required>
+			<input type="text" class="input{{ $errors->has('last_name') ? '-is-danger' : '' }}" name="last_name" placeholder="Last Name" value="{{old('last_name')}}" required>
 		</div>
 
 		<div>
-			<input type=number" name="age" value="{{old('age')}}" required>
+			<input type=number" class="input{{ $errors->has('age') ? '-is-danger' : '' }}" name="age" value="{{old('age')}}" required>
 		</div>
 
 		@if ($errors->any())
